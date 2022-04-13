57ef98f834290703e25f0cd6fed45fe4602b1af0
pankaj kore <korep95@gmail.com>
2022-04-13 23:19:57 +0530

    [pankaj]added greetingApp Project

---
+++ src/test/java/com/BridgeLabz/GrettingAppNew/GrettingAppNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BridgeLabz.GrettingAppNew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrettingAppNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
