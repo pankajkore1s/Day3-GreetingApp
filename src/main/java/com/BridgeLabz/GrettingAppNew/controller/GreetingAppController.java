a8ab18f8e7c2189a8fbb9959980a2ccb75c9d4d3
pankaj kore <korep95@gmail.com>
2022-04-14 00:04:51 +0530

    [pankaj]extended Controller for diff messages

--- src/main/java/com/BridgeLabz/GrettingAppNew/controller/GreetingAppController.java
+++ src/main/java/com/BridgeLabz/GrettingAppNew/controller/GreetingAppController.java
@@ -1,10 +1,7 @@
 package com.BridgeLabz.GrettingAppNew.controller;
 
 import com.BridgeLabz.GrettingAppNew.entity.Greeting;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 @RestController
@@ -18,5 +15,17 @@ public class GreetingAppController {
     @GetMapping("")
     public Greeting greeting(@RequestParam(value="name",defaultValue = "World")String name){
         return new Greeting(counter.incrementAndGet(),String.format(template,name));
+    }
+    @RequestMapping("/user")
+    public Greeting greetingUser(@RequestParam(value = "firstName")String firstName,
+                                 @RequestParam(value = "lastName")String lastName){
+        return new Greeting(counter.incrementAndGet(),String.format(template,(firstName+" "+lastName)));
+    }
+
+    @PutMapping("/{firstName}")
+    public Greeting greeting(@PathVariable String firstName,
+                             @RequestParam(value = "lastName")String lastName){
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template,(firstName+" "+lastName)));
     }
 }
